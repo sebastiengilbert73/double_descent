7148877f3d7b7e796814eb73d24e305dac7f8a98
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2023-11-18 12:43:09 -0500

    .\models\polynomial.py: Creates PolynomialWithGD

--- models/polynomial.py
+++ models/polynomial.py
@@ -1,3 +1,5 @@
+import torch
+
 class Polynomial:
     def __init__(self, coefficients=[0]):
         self.coefficients = coefficients
@@ -6,4 +8,15 @@ class Polynomial:
         sum = 0
         for power_ndx in range(len(self.coefficients)):
             sum += self.coefficients[power_ndx] * x**power_ndx
+        return sum
+
+class PolynomialWithGD(torch.nn.Module):
+    def __init__(self, coefficients=[0]):
+        super(PolynomialWithGD, self).__init__()
+        self.coefficients = torch.nn.Parameter(torch.tensor(coefficients))  # The coefficients will be registered for optimization
+
+    def forward(self, x_tsr):  # x_tsr.shape = (N)
+        sum = torch.zeros_like(x_tsr)
+        for power_ndx in range(len(self.coefficients)):
+            sum += self.coefficients[power_ndx] * torch.pow(x_tsr, power_ndx)#**power_ndx
         return sum
